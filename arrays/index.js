9a1d7642be4dedb93a14f59f1f6fd35afbbd69ff
raghav bhardwaj <braghav474@gmail.com>
2026-03-14 10:29:11 +0530

    basic methods of array

---
+++ arrays/index.js
@@ -0,0 +1,50 @@
+// how you can define an array
+
+const names = ["raghav","rahul","rohit"]
+const numbers = [1,2,3,4,5,6,7,8,9,10]
+const array = ["raghav",true,123,undefined,null]
+
+// console.log(names);
+console.log(array[3]);
+
+
+// array methods 
+
+// length = property
+// console.log(names.length);
+
+// methods
+
+
+// backend data => "sumit"
+  const newName = 'sumit'
+const names2 = ["raghav","rahul","rohit"]
+    // names2.push(newName)
+    // console.log(names2);
+
+    //  names2.pop()
+    //  console.log(names2); 
+
+
+    // hospital management system
+    names2.unshift(newName)
+    console.log("value after unshift",names2);
+    names2.shift()
+    console.log("value after shift",names2);
+    
+
+
+    //convert array into string => toString(),join()
+      const movies = ["avengers","juno","end game"]
+      console.log(movies.toString());
+       
+      console.log(movies.join(","));
+      
+
+    // at
+    console.log(movies.at(2));
+       
+
+       
+
+
